9246a73060b67b26af9ff343f3aa8967bf2ae811
itsneyoa <itstomsmart@icloud.com>
2021-04-10 01:28:51 +0100

    🚀 Rewrote

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const mineflayer = require('mineflayer');
-const chalk = require('chalk');
 const config = require('./config.json')
 
 const bot = mineflayer.createBot({
@@ -13,7 +12,7 @@ var loginAttempts = 0;
 var exactDelay = 0;
 
 bot.on('login', () => {
-  console.log(chalk.greenBright(`Minecraft bot ready, logged in as ${bot.username}`))
+  console.log(`Minecraft bot ready, logged in as ${bot.username}`)
 
   loginAttempts = 0;
   exactDelay = 0;
@@ -33,89 +32,96 @@ bot.on('end', () => {
       loginDelay = 60000
     }
   }
-  console.log(chalk.red(`Minecraft bot disconnected from server, attempting reconnect in ${loginDelay / 1000} seconds`))
+  console.log(`Minecraft bot disconnected from server, attempting reconnect in ${loginDelay / 1000} seconds`)
 
   setTimeout(() => bot.connect(), loginDelay);
 })
 
 bot.on('kicked', (reason) => {
-  console.log(chalk.red(`Minecraft bot was kicked from server for "${reason}"`));
+  console.log(`Minecraft bot was kicked from server for "${reason}"`);
 })
 
 bot.on('message', jsonMsg => {
   const message = jsonMsg.toString();
   console.log(message)
 
-  if (isLobbyJoinMessage(message)) sendToLimbo();
+  if (isLobbyJoinMessage(message)) {
+    return sendToLimbo()
+  }
 
-  if (isPartyMessage(message)) {
-    let partier = message.split('has', 1);
-    let hasRank = partier[0].includes('[');
-    var ign = '';
-    if (hasRank) {
-      ign = partier[0].split(" ")[1];
+  if (isPartyInviteMessage(message)) {
+    this.inviter = message.split(" ")[1]
+    if (this.inviter === "has") this.inviter = message.split(" ")[0].replace("-----------------------------\n", "") // Nons
+    if (config.blacklist.users.includes(this.inviter)) {
+      console.log("Won't accept party invite from: " + this.inviter + ", since they're on the blacklist")
+      let sorryMsg = `You're on the blacklist for fragruns.`
+      sorryMsg = addCharToString(sorryMsg, "⭍", 15);
+      bot.chat("/msg " + this.inviter + " " + sorryMsg)
     } else {
-      ign = partier[0];
+      console.log("Accepting party invite from: " + this.inviter)
+      setTimeout(() => {
+        bot.chat("/p join " + this.inviter)
+      }, 100)
     }
-    console.log(chalk.yellow(`Partying ${ign}`))
-    bot.chat(`/p accept ${ign}`)
+    return
   }
 
-  if (isPartyJoinMessage(message)) {
-    console.log(chalk.cyan(`Successfully joined a party!`));
-    bot.chat(`/pc Hi! I'm ${bot.username}. You can party me for frag runs - I'll auto leave after 10 seconds.`);
-    setTimeout(() => bot.chat(`/p leave`), config.settings.timeout);
-  }
+  if (this.inviter) {
+    if (isMessageYouJoinedParty(message)) {
+      console.log("Joined party from: " + this.inviter)
+      this.partyLeader = this.inviter
+      this.inviter = 0
 
-  if (isPartyLeaveMessage(message)) {
-    console.log(chalk.magenta(`Left the party`));
+      setTimeout(() => {
+        console.log("Leaving party from: " + this.partyLeader)
+        this.partyLeader = 0
+
+        bot.chat("/p leave")
+      }, 5000)
+    } else if (isMessageYoureInParty(message)) {
+      console.log("Can't join " + this.inviter + "'s party, already in a party with: " + this.partyLeader)
+      let pastInviter = this.inviter
+      this.inviter = 0
+
+      let sorryMsg = "Sorry, I'm already in a party with " + (this.partyLeader ? this.partyLeader : "someone") + ", try in a bit! uwu"
+
+      sorryMsg = addCharToString(sorryMsg, "⭍", 15);
+      bot.chat("/msg " + pastInviter + " " + sorryMsg)
+      setTimeout((pastInviter) => {
+        if (this.inviter === pastInviter || this.partyLeader == 0) bot.chat("/p leave") // In case it gets stuck
+      }, 5000)
+    }
   }
-  if (failedToJoinParty(message)) {
-    console.log(chalk.red(`Failed to join party`))
-  }
-  if (unkownCommand(message)) {
-    console.log(chalk.red(`Ran an unknown command`))
-  }
-  if (noIGN(message)) {
-    console.log(chalk.red('Failed to obtain username'))
-  }
-  if (failedAccept(message)) {
-    console.log(chalk.red('Failed to accept party invite'))
-  }
-});
+})
 
 function isLobbyJoinMessage(message) {
-  return (message.endsWith(' the lobby!') || message.endsWith(' the lobby! <<<')) && message.includes('[MVP+');
+  return (message.endsWith(' the lobby!') || message.endsWith(' the lobby! <<<')) && message.includes('[MVP+')
+}
+
+function isPartyInviteMessage(message) {
+  return message.endsWith(" here to join!\n-----------------------------") && !message.includes(':')
+}
+
+function isMessageYouJoinedParty(message) {
+  return message.endsWith(" party!") && !message.includes(':')
+}
+
+function isMessageYoureInParty(message) {
+  return message.endsWith(" to join another one.") && !message.includes(':')
+}
+
+function addCharToString(string, chars, times) {
+  for (let i = 0; i < times; i++) {
+    let randomNumber = Math.floor(Math.random() * string.length + 1)
+    let a = string.split("")
+
+    a.splice(randomNumber, 0, chars)
+    string = a.join("")
+  }
+  return string
 }
 
 function sendToLimbo() {
-  return bot.chat(`/ac §`);
-}
-
-function isPartyMessage(message) {
-  return message.includes('has invited you to join their party!');
-}
-
-function failedToJoinParty(message) {
-  return message.includes('That player does not exist.');
-}
-
-function isPartyJoinMessage(message) {
-  return message.startsWith(`You have joined `) && message.endsWith(`'s party!`);
-}
-
-function isPartyLeaveMessage(message) {
-  return message.startsWith(`You left the party.`) || message.startsWith(`The party was disbanded because all invites expired and the party was empty`);
-}
-
-function unkownCommand(message) {
-  return message.includes('Unknown command.');
-}
-
-function noIGN(message) {
-  return message.includes('Usage: /party accept <inviter>');
-}
-
-function failedAccept(message) {
-  return message.includes(`Couldn't find a player with that name`)
+  console.log(`Sending client to limbo`)
+  return bot.chat('limbowo')
 }
